cee94a61bc8be6d2a34958ee9fc8a7df44b392ce
Anjaneya Kumar <131331479+Anjaneyakumar18@users.noreply.github.com>
2025-05-19 14:08:45 +0530

    Create Merge sort.py

---
+++ D & C/Merge sort.py
@@ -0,0 +1,29 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        def merge_sort(arr):
+            if len(arr) <= 1:
+                return arr
+            
+            mid = len(arr) // 2
+            left = merge_sort(arr[:mid])
+            right = merge_sort(arr[mid:])
+            
+            return merge(left, right)
+        
+        def merge(left, right):
+            result = []
+            i = j = 0
+            
+            while i < len(left) and j < len(right):
+                if left[i] < right[j]:
+                    result.append(left[i])
+                    i += 1
+                else:
+                    result.append(right[j])
+                    j += 1
+            
+            result.extend(left[i:])
+            result.extend(right[j:])
+            return result
+        
+        return merge_sort(nums)
