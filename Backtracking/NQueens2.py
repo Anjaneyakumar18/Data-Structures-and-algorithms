b291c02587a05a19041539842d28e197365b2a59
Anjaneya Kumar <131331479+Anjaneyakumar18@users.noreply.github.com>
2025-05-19 14:03:01 +0530

    Create NQueens2.py

---
+++ Backtracking/NQueens2.py
@@ -0,0 +1,29 @@
+class Solution:
+    def totalNQueens(self,n:int)->List[List[str]]:
+        def is_valid(row,col):
+            return col not in cols and (row+col) not in diag1 and (row-col) not in diag2
+
+        def backtrack(row,board):
+            if row==n:
+                nonlocal result
+                result+=1
+                return
+            for col in range(n):
+                if is_valid(row,col):
+                    cols.add(col)
+                    diag1.add(row+col)
+                    diag2.add(row-col)
+                    board[row][col]='Q'
+                    backtrack(row+1,board)
+                    board[row][col]='.'
+                    cols.remove(col)
+                    diag1.remove(row+col)
+                    diag2.remove(row-col)
+
+        result=0
+        cols=set()
+        diag1=set()
+        diag2=set()
+        board=[['.']*n for _ in range(n)]
+        backtrack(0,board)
+        return result
